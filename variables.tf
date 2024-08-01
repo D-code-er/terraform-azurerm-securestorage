fdcbc9deede24c5d58377dccf91dc4c5053fd2e6
d-code-er <ddolo@hotmail.com>
2024-08-01 20:33:23 +0000

    version 1.0.0

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "name" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "location" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
